27bfbd7ad5e43fba39d97a40569c8eab07cbc7d9
黄旭 <495921109@qq.com>
2023-03-05 21:58:30 +0800

    init rust

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("msg");
+
+    println!("You guessed: {guess}");
+}
+
+
+ 
